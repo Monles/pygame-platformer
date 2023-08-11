9fd83357075ea9a0893dfee7e58c1c2b922a7a95
Monles <iceueb@gmail.com>
2023-08-11 12:14:12 +0000

    update

--- game.py
+++ game.py
@@ -2,23 +2,29 @@ import sys
 
 import pygame
 
+
 class Game:
     def __init__(self):
         pygame.init()
 
-        pygame.display.set_caption('ninja game')
+        pygame.display.set_caption('Pygame Platformer')
         self.screen = pygame.display.set_mode((640, 480))
 
         self.clock = pygame.time.Clock()
-        
+
+        self.img = pygame.image.load('data/images/clouds/cloud_1.png')
+
     def run(self):
         while True:
+            self.screen.blit(self.img, (100, 200))
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-            
+
             pygame.display.update()
             self.clock.tick(60)
+
 
 Game().run()
